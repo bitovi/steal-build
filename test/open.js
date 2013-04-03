22873a89f143403c0daa97a80827917be459eff4
David Luecke <daff@neyeon.com>
2013-04-03 15:27:44 -0600

    Make opener Steal settings configurable

--- test/open.js
+++ test/open.js
@@ -3,7 +3,7 @@ var opener = require('./../lib/build/open');
 
 describe('Open', function(){
 	it('Opens a dependency and gives me the root steal', function(done) {
-		opener('hello', __dirname, function(error, steals) {
+		opener('hello', { root: __dirname }, function(error, steals) {
 			assert.equal(steals.length, 1);
 			assert.equal(steals[0].options.id.toString(), 'hello/hello.js');
 			assert.equal(steals[0].dependencies.length, 3);
