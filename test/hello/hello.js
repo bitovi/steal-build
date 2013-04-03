07565bb31e7dfbf399e8de9cad1c9820ccef9e16
David Luecke <daff@neyeon.com>
2013-04-03 16:17:42 -0600

    Steal configuration tests

--- test/hello/hello.js
+++ test/hello/hello.js
@@ -1,7 +1,7 @@
 // A comment
 var test;
 
-steal('./world.js', './other.js', function(test, other) {
+steal('hello/world.js', './other.js', function(test, other) {
 	var content = "Hello ";
 
 	return content + test + other;
--- test/pluginify.js
+++ test/pluginify.js
@@ -2,7 +2,34 @@ var pluginify = require('./../lib/build/pluginify');
 var assert = require('assert');
 
 describe('Pluginify', function(){
-	it('Pluginifies the hello fixture', function(done) {
+//	it('Pluginifies the hello fixture', function(done) {
+//		var exporter = {};
+//
+//		// Run pluginify on hello/hello.js in the current folder.
+//		// Export it to pluginifyMessage
+//		// Set the wrapper so that it will be added to the exporter object
+//		pluginify('hello', {
+//			steal: {
+//				root: __dirname
+//			},
+//			exports: {
+//				'hello': 'pluginifyMessage',
+//				'hello/world.js': 'world'
+//			},
+//			wrapper: '!function(window, undefined) {\n<%= content %>\n\n' +
+//				'<%= exports.join("\\n") %>\n' +
+//				'}(exporter);'
+//		}, function(error, content) {
+//			// Run the pluginified content
+//			eval(content);
+//			// And make sure that the exported object got updated
+//			assert.equal(exporter.pluginifyMessage, 'Hello World WORLD!');
+//			assert.equal(exporter.world, 'World');
+//			done();
+//		});
+//	});
+
+	it('Pluginifies with Steal configuration', function(done) {
 		var exporter = {};
 
 		// Run pluginify on hello/hello.js in the current folder.
@@ -11,18 +38,24 @@ describe('Pluginify', function(){
 		pluginify('hello', {
 			root: __dirname,
 			exports: {
-				'hello': 'pluginifyMessage',
-				'hello/world.js': 'world'
+				'hello': 'pluginifyMessage'
 			},
 			wrapper: '!function(window, undefined) {\n<%= content %>\n\n' +
 				'<%= exports.join("\\n") %>\n' +
-				'}(exporter);'
+				'}(exporter);',
+			steal: {
+				root: __dirname,
+				map: {
+					'*': {
+						'hello/world.js': 'hello/mapped.js'
+					}
+				}
+			}
 		}, function(error, content) {
 			// Run the pluginified content
 			eval(content);
 			// And make sure that the exported object got updated
-			assert.equal(exporter.pluginifyMessage, 'Hello World WORLD!');
-			assert.equal(exporter.world, 'World');
+			assert.equal(exporter.pluginifyMessage, 'Hello Mars MARS!');
 			done();
 		});
 	});
