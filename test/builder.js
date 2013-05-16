ab01b505ed9f0ef9c527fc1e355d73e21cd08fcc
David Luecke <daff@neyeon.com>
2013-05-16 16:07:48 -0600

    Refactoring for builder with na little nicer API. Removing minification code, should be handled by user.

--- test/builder.js
+++ test/builder.js
@@ -3,29 +3,46 @@ var assert = require('assert');
 var _ = require('underscore');
 
 describe('Steal download builder tests', function() {
+	it('builder.banner', function() {
+		var json = require(__dirname + '/fixture/builder.json');
+		var banner = builder.banner(json.banner, { info: { name: 'Bannertest' } });
+		assert.equal(banner, '/*! Banner: Bannertest */\n');
+	});
+
+	it('builder.prettify', function() {
+		var source = "/** some comment */\n\n\n// another one\nfunction() { var x = 1; if(true) { return;;; } }";
+		var expected = "// another one\n\nfunction() {\n\tvar x = 1;\n\tif (true) {\n\t\treturn;\n\t}\n}";
+		assert.equal(builder.prettify(source, {
+			"indent_size": 1,
+			"indent_char": "\t"
+		}), expected);
+	});
+
 	it('loads the fixture package.json and builder.json and passes info', function(done) {
-		builder.loadInfo(__dirname + '/fixture', function(error, info) {
+		builder(__dirname + '/fixture', function(error, info, build) {
 			assert.ok(!error);
 			assert.equal(info.name, 'builder-test');
 			assert.equal(info.filename, 'builder.custom.js');
 			assert.equal(_.values(info.modules).length, 2);
+			assert.equal(typeof build, 'function');
 			done();
 		});
 	});
 
 	it('loads all dependencies and their content per configuration', function(done) {
-		builder.loadInfo(__dirname + '/fixture', function(error, info) {
+		builder(__dirname + '/fixture', function(error, info, build) {
 			assert.ok(!error);
 			assert.equal(info.configurations.dummy.steals.length, 2);
 			assert.equal(info.configurations.mapped.steals.length, 2);
 			assert.ok(info.configurations.dummy.steals[0].options.text);
+			assert.equal(typeof build, 'function');
 			done();
 		});
 	});
 
 	it('builds with default configuration', function(done) {
-		builder.loadInfo(__dirname + '/fixture', function(error, info) {
-			builder({}, info, function(error, content) {
+		builder(__dirname + '/fixture', function(error, info, build) {
+			build({}, function(error, content) {
 				var exporter = {};
 				eval(content);
 				assert.equal(exporter.pluginified, 'Hello World WORLD!');
@@ -36,17 +53,17 @@ describe('Steal download builder tests', function() {
 	});
 
 	it('builds using configuration with mappings and exports properly', function(done) {
-		builder.loadInfo(__dirname + '/fixture', function(error, info) {
-			builder({
+		builder(__dirname + '/fixture', function(error, info, build) {
+			build({
 				ids: ['fixture/hello'],
 				configuration: 'mapped'
-			}, info, function(error, content) {
+			}, function(error, content) {
 				var exporter = {};
 				eval(content);
 				assert.equal(exporter.pluginified, 'Hello Mars MARS!');
 				assert.equal(exporter.planet, 'Mars');
 				done();
-			})
+			});
 		});
 	});
 });
